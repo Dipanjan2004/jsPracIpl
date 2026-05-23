347ffcc858841ebe382c5b57fd72224786b10df2
Dipanjan Chowdhury <chowdhurydipanjan173@gmail.com>
2026-05-23 12:10:55 +0530

    fix images and add better ui

--- script.js
+++ script.js
@@ -1,50 +1,114 @@
-var arr=[
+const teams = [
     {
-        Team:'KOLKATA KNIGHT RIDERS',
-        Color:'Purple',
-        bgImg: 'https://img.jagranjosh.com/images/2022/March/2632022/KKR_2022.jpg'
-    },
-     {
-        Team:'MUMBAI INDIANS',
-        Color:'Blue',
-        bgImg: 'https://d28wu8o6itv89t.cloudfront.net/images/MumbaiIndianspng-1598599799716.png'
+        name: "Kolkata Knight Riders",
+        shortName: "KKR",
+        colors: "Purple and gold",
+        primary: "#32105f",
+        secondary: "#f6c64b",
+        logo: "https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Kolkata_Knight_Riders_Logo.svg/250px-Kolkata_Knight_Riders_Logo.svg.png"
     },
     {
-        Team:'ROYAL CHALLENGERS BANGALORE',
-        Color:'Red',
-        bgImg: 'https://www.crictracker.com/wp-content/uploads/2022/03/RCB-2022.jpg'
+        name: "Mumbai Indians",
+        shortName: "MI",
+        colors: "Blue and gold",
+        primary: "#004ba8",
+        secondary: "#ffb81c",
+        logo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/Mumbai_Indians_Logo.svg/250px-Mumbai_Indians_Logo.svg.png"
     },
-        {
-        Team:'DELHI CAPITALS',
-        Color:'Blue',
-        bgImg: 'https://www.crictracker.com/wp-content/uploads/2022/03/DelhiCapitals-2022.jpg'
+    {
+        name: "Royal Challengers Bengaluru",
+        shortName: "RCB",
+        colors: "Red, black and gold",
+        primary: "#7b1018",
+        secondary: "#d5b45b",
+        logo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Royal_Challengers_Bengaluru_Logo.svg/250px-Royal_Challengers_Bengaluru_Logo.svg.png"
     },
-        {
-        Team:'SUNRISERS HYDERABAD',
-        Color:'Orange',
-        bgImg: 'https://www.crictracker.com/wp-content/uploads/2022/03/SRH-2022.jpg'
+    {
+        name: "Delhi Capitals",
+        shortName: "DC",
+        colors: "Blue and red",
+        primary: "#163c8c",
+        secondary: "#e51f35",
+        logo: "https://upload.wikimedia.org/wikipedia/en/thumb/2/2f/Delhi_Capitals.svg/250px-Delhi_Capitals.svg.png"
     },
-        {
-        Team:'PUNJAB KINGS',
-        Color:'Red',
-        bgImg: 'https://www.crictracker.com/wp-content/uploads/2022/03/PunjabKings-2022.jpg'
+    {
+        name: "Sunrisers Hyderabad",
+        shortName: "SRH",
+        colors: "Orange and black",
+        primary: "#e75b12",
+        secondary: "#ffcf42",
+        logo: "https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Sunrisers_Hyderabad_Logo.svg/250px-Sunrisers_Hyderabad_Logo.svg.png"
     },
-        {
-        Team:'RAJASTHAN ROYALS',
-        Color:'Pink',
-        bgImg: 'https://www.crictracker.com/wp-content/uploads/2022/03/RR-2022.jpg'
+    {
+        name: "Punjab Kings",
+        shortName: "PBKS",
+        colors: "Red and gold",
+        primary: "#d71920",
+        secondary: "#d8b56d",
+        logo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Punjab_Kings_Logo.svg/250px-Punjab_Kings_Logo.svg.png"
+    },
+    {
+        name: "Rajasthan Royals",
+        shortName: "RR",
+        colors: "Pink and royal blue",
+        primary: "#d8137f",
+        secondary: "#2456a6",
+        logo: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5c/This_is_the_logo_for_Rajasthan_Royals%2C_a_cricket_team_playing_in_the_Indian_Premier_League_%28IPL%29.svg/250px-This_is_the_logo_for_Rajasthan_Royals%2C_a_cricket_team_playing_in_the_Indian_Premier_League_%28IPL%29.svg.png"
     }
-]
+];
 
-btn=document.querySelector('button');
-h1=document.querySelector('#box>h1');
-main=document.querySelector('#main')
+const button = document.querySelector("#change-team");
+const main = document.querySelector("#main");
+const teamName = document.querySelector("#team-name");
+const teamColor = document.querySelector("#team-color");
+const teamTag = document.querySelector("#team-tag");
+const teamLogo = document.querySelector("#team-logo");
+const teamFallback = document.querySelector("#team-fallback");
 
+let lastTeamIndex = -1;
 
-btn.addEventListener('click',function(){
+function pickRandomTeam() {
+    let index = Math.floor(Math.random() * teams.length);
 
-    var num=arr[Math.floor(Math.random()*7)];
-    h1.innerHTML=num.Team;
-    main.style.backgroundImage=`url(${num.bgImg})`
+    if (teams.length > 1) {
+        while (index === lastTeamIndex) {
+            index = Math.floor(Math.random() * teams.length);
+        }
+    }
 
-})
+    lastTeamIndex = index;
+    return teams[index];
+}
+
+function showFallback(team) {
+    teamLogo.classList.add("is-hidden");
+    teamFallback.textContent = team.shortName;
+    teamFallback.classList.add("is-visible");
+}
+
+function showTeam(team) {
+    main.style.setProperty("--team-primary", team.primary);
+    main.style.setProperty("--team-secondary", team.secondary);
+
+    teamName.textContent = team.name;
+    teamColor.textContent = team.colors;
+    teamTag.textContent = team.shortName;
+    teamFallback.classList.remove("is-visible");
+    teamLogo.classList.remove("is-hidden");
+
+    teamLogo.alt = `${team.name} logo`;
+    teamLogo.onerror = function () {
+        showFallback(team);
+    };
+    teamLogo.onload = function () {
+        teamFallback.classList.remove("is-visible");
+        teamLogo.classList.remove("is-hidden");
+    };
+    teamLogo.src = team.logo;
+}
+
+button.addEventListener("click", function () {
+    showTeam(pickRandomTeam());
+});
+
+showTeam(pickRandomTeam());
